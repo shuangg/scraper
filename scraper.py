5f6f4c245458dff2f662627868e7e021ee1965f0
Shuang Gao <shuangg@gmail.com>
2024-02-14 13:38:11 +0000

    init

---
+++ scraper.py
@@ -0,0 +1,51 @@
+
+# https://www.youtube.com/watch?v=dYWnS8eRf4M
+# Building a CLI Tool | Scraping & Processing Data with BeautifulSoup, Requests & Pydantic
+import requests
+from bs4 import BeautifulSoup, Tag
+from pprint import pprint
+from pydantic import BaseModel, AnyHttpUrl
+from datetime import date
+
+class Episode(BaseModel):
+    show_number: int
+    date: date
+    title: str
+    url: AnyHttpUrl
+    guest: str
+
+def extract_episode_data(row: Tag) -> dict:
+    model_data = {}
+    row_data = row.select( 'td')
+    for i, td in enumerate(row_data):
+        if i == 0:
+            model_data['show_number'] = td.text.replace('#', '')
+        elif i == 1:
+            model_data['date'] = td.text
+        elif i == 2:
+            link = td.find('a')
+            model_data['url'] = base_url + link.attrs['href']
+            model_data['title'] = link.text
+        elif i == 3:
+            model_data['guest'] = td.text
+    return model_data
+
+base_url = 'https://talkpython.fm'
+url = 'https://talkpython.fm/episodes/all'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+rows = soup.select('tbody > tr')
+# episodes = []
+# for row in rows:
+#     data = extract_episode_data(row)
+#     episodes.append(Episode(**data))
+# As a one-liner
+episodes = [Episode(**extract_episode_data(row)) for row in rows]
+
+search_term = input('Enter a search term: ')
+results = [episode for episode in episodes if search_term.lower() in episode.title.lower()]
+
+print(f"Found {len(results)} episodes with the term {search_term}")
+pprint(results)
